1aa5913d074ce80cd7c8b085034ad03f48b5f311
brooke <brookefuller96@knights.ucf.edu>
2021-06-06 22:05:08 -0400

    /*  *  UCF COP3330 Summer 2021 Assignment 1 Solution  *  Copyright 2021 Brooke Fuller  */

--- src/main/java/Exercise8.java
+++ src/main/java/Exercise8.java
@@ -1,7 +1,12 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Brooke Fuller
+ */
+
 import java.util.Scanner;
 import java.lang.*;
-import java.time.*;
 import static java.lang.System.*;
+
 public class Exercise8 {
     public static void main(String[] args) {
 
